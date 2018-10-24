7ea4c5501b7bb5d7707fe05e2bf22d3a2d737096
elpupper <Datboi@s-MacBook-Air.local>
2018-10-24 20:56:56 +0400

    this is my very first commitment

--- Opengl/Shaders/frag.fs
+++ Opengl/Shaders/frag.fs
@@ -1,10 +1,19 @@
 #version 410 core
 in vec2 newTexture;
+in vec3 fragNormal;
 
 out vec4 outColor;
 uniform sampler2D samplerTexture;
 
 void main()
 {
-    outColor = texture(samplerTexture, newTexture);
+	vec3 ambientLI = vec3(0.3f, 0.2f, 0.4f);
+	vec3 sunLI = vec3(0.9f, 0.9f, 0.9f);
+	vec3 sunLD = normalize(vec3(0.0f, 0.0f, 2.0f));
+	
+	vec4 texel = texture(samplerTexture, newTexture);
+	
+	vec3 LI = ambientLI + sunLI * max(dot(fragNormal, sunLD), 0.0f);
+	
+    outColor = vec4(texel.rgb * LI, texel.a);
 }
--- Opengl/main.py
+++ Opengl/main.py
@@ -7,17 +7,42 @@ from OBJLoader import *
 from camera import Camera
 from math import sin,cos
 import TextureLoader
+import os
 
 
 keys = [False] * 1024
 cam = Camera()
 lastX, lastY = 720, 450
 first_mouse = True
-filename = "epic.jpg"
+filename = "Screenshot_1.jpg"
+
+mouse_x,mouse_y = 0,0
+
+def render_to_jpg(format="JPEG"):
+    global filename
+    os.chdir(r"/Users/Datboi/Desktop/OGL/Screenshots")
+    for file in os.listdir(os.curdir):
+        if file == filename:
+            name, ext = file.split(".")
+            word, number = name.split("_")
+            new_num = int(number) + 1
+            filename = word + "_" + str(new_num) + "." + ext
+        else:
+            continue
+        
+    x,y,width,height = glGetDoublev(GL_VIEWPORT)
+    width,height = int(width), int(height)
+    glPixelStorei(GL_PACK_ALIGNMENT,1)
+    data = glReadPixels(x,y,width,height,GL_RGB,GL_UNSIGNED_BYTE)
+    image = Image.frombytes("RGB",(width,height),data)
+    image = image.transpose(Image.FLIP_TOP_BOTTOM)
+    image.save(filename, format)
 
 def key_callback(window, key, scancode, action, mode):
     if key == glfw.KEY_ESCAPE and action == glfw.PRESS:
         glfw.set_window_should_close(window, True)
+    if key == glfw.KEY_F12 and action == glfw.PRESS:
+        render_to_jpg()
 
     if key >= 0 and key < 1024:
         if action == glfw.PRESS:
@@ -34,6 +59,15 @@ def do_movement():
         cam.process_keyboard("LEFT", 0.1)
     if keys[glfw.KEY_D]:
         cam.process_keyboard("RIGHT", 0.1)
+
+def cursor_pos_callback(window, xpos, ypos):
+    global mouse_x, mouse_y
+    width,height = glfw.get_window_size(window)
+    mouse_x = xpos
+    mouse_y = height - ypos
+
+def mouse_button_callback(window,button,action,mods):
+    data = glReadPixels(mouse_x, mouse_y, 1, 1, GL_RGB, GL_FLOAT)
 
 def mouse_callback(window, xpos, ypos):
     global first_mouse,lastX, lastY
@@ -78,11 +112,13 @@ def main():
     monkey_obj.load_model('monkey.obj')
     monkey_tex = TextureLoader.load_texture("res/monkey.png")
     monkey_tex_offset = len(monkey_obj.vertex_index)*12
+    monkey_norm_offset = (monkey_tex_offset + len(monkey_obj.texture_index) * 8)
 
     sphere_obj = ObjLoader()
     sphere_obj.load_model('sphere2.obj')
     sphere_tex = TextureLoader.load_texture("res/yellow.png")
     sphere_tex_offset = len(sphere_obj.vertex_index)*12
+    sphere_norm_offset = (sphere_tex_offset + len(sphere_obj.texture_index) * 8)
 
     shader = ShaderLoader.compile_shader("Shaders/vert.vs", "Shaders/frag.fs")
 
@@ -97,6 +133,9 @@ def main():
     #TextureCoords
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sphere_obj.model.itemsize * 2, ctypes.c_void_p(sphere_tex_offset))
     glEnableVertexAttribArray(1)
+    #Normals
+    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sphere_obj.model.itemsize * 3, ctypes.c_void_p(sphere_norm_offset))
+    glEnableVertexAttribArray(2)
     glBindVertexArray(0)
 
     monkey_vao = glGenVertexArrays(1)
@@ -110,6 +149,9 @@ def main():
     #TextureCoords
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, monkey_obj.model.itemsize * 2, ctypes.c_void_p(monkey_tex_offset))
     glEnableVertexAttribArray(1)
+    #Normals
+    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, monkey_obj.model.itemsize * 3, ctypes.c_void_p(monkey_norm_offset))
+    glEnableVertexAttribArray(2)
     glBindVertexArray(0)
     
     glClearColor(0.2, 0.3, 0.2, 1.0)
@@ -124,6 +166,7 @@ def main():
     model_loc = glGetUniformLocation(shader, "model")
     view_loc = glGetUniformLocation(shader, "view")
     proj_loc = glGetUniformLocation(shader, "proj")
+    light_loc = glGetUniformLocation(shader, "light")
     
     glUniformMatrix4fv(proj_loc, 1, GL_FALSE, projection)
     
@@ -136,7 +179,10 @@ def main():
         view = cam.get_view_matrix()
         glUniformMatrix4fv(view_loc, 1, GL_FALSE, view)
 
+        light_y = Matrix44.from_y_rotation(0.5)
         rot_y = Matrix44.from_y_rotation(glfw.get_time() * 0.5)
+        
+        glUniformMatrix4fv(light_loc, 1, GL_FALSE, light_y)
 
         glBindVertexArray(sphere_vao)
         glBindTexture(GL_TEXTURE_2D, sphere_tex)
